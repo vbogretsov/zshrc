54a12839a346e3be66b45b19350ad789cfa433f3
Bogretsov <bogrecov@gmail.com>
2019-09-22 18:37:04 +0300

    added vi mode

---
+++ plugins/vimode/vimode.plugin.zsh
@@ -0,0 +1,13 @@
+# https://unix.stackexchange.com/questions/547/make-my-zsh-prompt-show-mode-in-vi-mode/1019
+
+bindkey -v
+
+function zle-line-init zle-keymap-select {
+    RPROMPT=${${KEYMAP/vicmd/'[vi]'}/(main|viins)/''}
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+bindkey "^G" vi-cmd-mode
